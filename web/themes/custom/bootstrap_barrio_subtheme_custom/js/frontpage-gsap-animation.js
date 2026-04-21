708108ff72dbb6f22ddf2c43e55c963c22bfe170
Picology-co <picology.co@gmail.com>
2026-04-21 13:42:11 +0900

    main

--- web/themes/custom/bootstrap_barrio_subtheme_custom/js/frontpage-gsap-animation.js
+++ web/themes/custom/bootstrap_barrio_subtheme_custom/js/frontpage-gsap-animation.js
@@ -1,2 +1,9 @@
 // フロントページのGSAPアニメーション
 
+const tl = gsap.timeline();
+
+gsap.to(".green", { rotation: 360, x: 400, duration: 2 });
+
+gsap.from(".purple", { rotation: -360, x: -200, duration: 3 });
+
+gsap.fromTo(".blue", { x: -100 }, { rotation: 360, x: 800, duration: 5 });
