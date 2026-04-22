8bc948f2845ef15a757cd63e0c986686a3416306
Picology-co <picology.co@gmail.com>
2026-04-22 13:13:15 +0900

    main

--- web/themes/custom/bootstrap_barrio_subtheme_custom/js/frontpage-gsap-animation.js
+++ web/themes/custom/bootstrap_barrio_subtheme_custom/js/frontpage-gsap-animation.js
@@ -1,10 +1,18 @@
 // フロントページのGSAPアニメーション
 
 // ステップ1
-gsap.from("div.animation__step1__image", {
+gsap.from("div.animation__step1__image down", {
     y: -1024,
     opacity: 0,
-    duration: 3.0,
+    duration: 1.2,
     ease: "power2.out",
-    stagger: 3.0     // 0.2秒ずつずらして表示
+    stagger: 3.0
+});
+
+gsap.from("div.animation__step1__image up", {
+    y: 1024,
+    opacity: 0,
+    duration: 1.2,
+    ease: "power2.out",
+    stagger: 3.0
 });
