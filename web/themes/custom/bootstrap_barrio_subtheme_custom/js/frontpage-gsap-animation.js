40ff2d21d50c6e5e45ebfb16f62258d3da363bf0
Picology-co <picology.co@gmail.com>
2026-04-27 09:41:19 +0900

    main

--- web/themes/custom/bootstrap_barrio_subtheme_custom/js/frontpage-gsap-animation.js
+++ web/themes/custom/bootstrap_barrio_subtheme_custom/js/frontpage-gsap-animation.js
@@ -98,7 +98,7 @@ function flipCover3D(target, zIndex) {
     }, "<");
 
     tl.to(target, {
-    rotationY: -177,
+    rotationY: -180,
     rotationX: 0,
     z: 0,
     y: 0,
@@ -178,7 +178,7 @@ function flipPage3D(target, zIndex, options = {}) {
     }, "<");
 
     tl.to(target, {
-    rotationY: -177,
+    rotationY: -180,
     rotationX: 0,
     z: 0,
     y: 0,
